b77855a8f88d535f7409ba78b012bbb2015a2477
Cubemaster7 <lukehamel7@gmail.com>
2016-09-09 09:07:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #Orbic Text Adventure 2016 Luke Hamel 
 #Connor, if you're seeing this, put your name on that top line and delete this
-print("Welcome to Orbic Legacy.");
-response = input("Do you want to play the tutorial? (Type 'Yes' or 'No') ");
-if response == "yes"
-  print("Ayy lmao");
+print("Welcome to Orbic Legacy.")
+def tutorial():
+  response = input("Do you want to play the tutorial? (Type 'Yes' or 'No') ")
+  if response == "yes" or response=="Yes" or response=="YES":
+    print("Ayy lmao")
+  elif response == "no" or response=="No" or response=="NO":
+    
+  else:
+    tutorial()
 
 
