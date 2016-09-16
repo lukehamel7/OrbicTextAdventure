d0b39c2c5ae059f1d7103838ed75404f1ebb233b
Cubemaster7 <lukehamel7@gmail.com>
2016-09-16 09:17:49 -0400

    Update shipCombatTest.py

--- shipCombatTest.py
+++ shipCombatTest.py
@@ -4,9 +4,21 @@ def shipCombat(playerX,playerY,playerD,enemyX,enemyY,enemyD): #So this is just a
   rowC = [0,0,0,0,0]
   rowD = [0,0,0,0,0]
   rowE = [0,0,0,0,0]
+  if playerY == 1:
+    del rowA[playerX-1]
+    rowA.insert(playerX-1, ">")
+  if playerY == 2:
+    del rowB[playerX-1]
+    rowB.insert(playerX-1, ">")
   if playerY == 3:
     del rowC[playerX-1]
     rowC.insert(playerX-1, ">")
+  if playerY == 4:
+    del rowD[playerX-1]
+    rowD.insert(playerX-1, ">")
+  if playerY == 5:
+    del rowE[playerX-1]
+    rowE.insert(playerX-1, ">")
   print(rowA[0], rowA[1], rowA[2], rowA[3], rowA[4])
   print(rowB[0], rowB[1], rowB[2], rowB[3], rowB[4])
   print(rowC[0], rowC[1], rowC[2], rowC[3], rowC[4])
@@ -14,7 +26,9 @@ def shipCombat(playerX,playerY,playerD,enemyX,enemyY,enemyD): #So this is just a
   print(rowE[0], rowE[1], rowE[2], rowE[3], rowE[4])
   start()
 def start():
-  starting = input("What position should the ship be in?")
+  starting = input("What X position should the ship be in? (1-5)")
+  starting2 = input("What Y position should the ship be in? (1-5)")
   starting = int(starting)
-  shipCombat(starting,3,1,1,1,1)
+  starting2 = int(starting2)
+  shipCombat(starting,starting2,1,1,1,1)
 start()
