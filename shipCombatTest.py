f3d97f40201a816f4bd5e1a981a25b9cd5fddbf2
Cubemaster7 <lukehamel7@gmail.com>
2016-09-19 10:20:16 -0400

    Update shipCombatTest.py

--- shipCombatTest.py
+++ shipCombatTest.py
@@ -1,24 +1,24 @@
 def shipCombat(playerX,playerY,playerD,enemyX,enemyY,enemyD): #So this is just a test, pretty hard to explain but hopefully I can get it to work. 
-  rowA = ["#","#","#","#","#"]
-  rowB = ["#","#","#","#","#"]
-  rowC = ["#","#","#","#","#"]
-  rowD = ["#","#","#","#","#"]
-  rowE = ["#","#","#","#","#"]
+  rowA = ["-","-","-","-","-"]
+  rowB = ["-","-","-","-","-"]
+  rowC = ["-","-","-","-","-"]
+  rowD = ["-","-","-","-","-"]
+  rowE = ["-","-","-","-","-"]
   if playerY == 1:
     del rowA[playerX-1]
-    rowA.insert(playerX-1, ">")
+    rowA.insert(playerX-1, "*")
   if playerY == 2:
     del rowB[playerX-1]
-    rowB.insert(playerX-1, ">")
+    rowB.insert(playerX-1, "*")
   if playerY == 3:
     del rowC[playerX-1]
-    rowC.insert(playerX-1, ">")
+    rowC.insert(playerX-1, "*")
   if playerY == 4:
     del rowD[playerX-1]
-    rowD.insert(playerX-1, ">")
+    rowD.insert(playerX-1, "*")
   if playerY == 5:
     del rowE[playerX-1]
-    rowE.insert(playerX-1, ">")
+    rowE.insert(playerX-1, "*")
   print(rowA[0], rowA[1], rowA[2], rowA[3], rowA[4])
   print(rowB[0], rowB[1], rowB[2], rowB[3], rowB[4])
   print(rowC[0], rowC[1], rowC[2], rowC[3], rowC[4])
